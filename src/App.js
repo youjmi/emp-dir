737121c74c64ebc3e4759e7761a92fbaf7439f98
Young Ji Kim <youjmik@gmail.com>
2021-03-30 16:35:33 -0400

    starting on adding structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Search from "./pages/Search";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/search" component={Search} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Search.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import API from "../utils/API";
+import Container from "../components/Container";
+import SearchForm from "../components/SearchForm";
+import SearchResults from "../components/SearchResults";
+import Alert from "../components/Alert";
+
+class Search extends Component {
+  state = {
+    search: "",
+    breeds: [],
+    results: [],
+    error: ""
+  };
+
+  // // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  // componentDidMount() {
+  //   API.search()
+  //     .then(res => this.setState({ results: res.data.message }))
+  //     .catch(err => console.log(err));
+  // }
+
+  // handleInputChange = event => {
+  //   this.setState({ search: event.target.value });
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   this.setState(this.state.search);
+  // }
+    
+  render() {
+    return (
+      <div>
+        <Container style={{ minHeight: "80%" }}>
+          <h1 className="text-center">Search By Breed!</h1>
+          <Alert
+            type="danger"
+            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+          >
+            {this.state.error}
+          </Alert>
+          <SearchForm
+            handleFormSubmit={this.handleFormSubmit}
+            handleInputChange={this.handleInputChange}
+            breeds={this.state.breeds}
+          />
+          <SearchResults results={this.state.results} />
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default Search;
