7fa100559e6fe47e6d6a64076c91858485014029
Young Ji Kim <youjmik@gmail.com>
2021-03-30 17:34:48 -0400

    touching up on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -20,39 +20,32 @@ function Home() {
         <Row>
           <Col size="md-12">
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc aliquet diam tortor, id
-              consequat mauris ullamcorper eu. Orci varius natoque penatibus et magnis dis
-              parturient montes, nascetur ridiculus mus. Pellentesque et dui id justo finibus
-              sollicitudin at et metus. Ut feugiat tellus nec metus commodo, sed suscipit nisi
-              gravida. Duis eget vestibulum quam, ut porttitor sem. Donec sagittis mi sollicitudin
-              turpis semper, et interdum risus lobortis. Vestibulum suscipit nunc non egestas
-              tristique. Proin hendrerit efficitur malesuada. Mauris lorem urna, sodales accumsan
-              quam non, tristique tempor erat. Nullam non sem facilisis, tempus tortor sit amet,
-              volutpat nisl. Ut et turpis non nunc maximus mollis a vitae tortor. Pellentesque
-              mattis risus ac quam laoreet cursus. Praesent suscipit orci neque, vestibulum
-              tincidunt augue tincidunt non. Duis consequat mattis tortor vitae mattis.
-            </p>
-            <p>
-              Phasellus at rutrum nisl. Praesent sed massa ut ipsum bibendum porttitor. Sed
-              malesuada molestie velit ac viverra. Quisque a ullamcorper purus. Curabitur luctus mi
-              ac mi hendrerit semper. Nulla tincidunt accumsan lobortis. Mauris convallis sapien non
-              nibh porta accumsan. Nunc volutpat tempus porttitor. Nunc congue dictum egestas.
-              Aliquam blandit mi eu urna scelerisque, vitae volutpat ligula ultricies. Maecenas vel
-              porta augue. Fusce mauris ex, dignissim et lacinia ut, tempus eget nibh.
-            </p>
-            <p>
-              Etiam ut massa efficitur, gravida sapien non, condimentum sapien. Suspendisse massa
-              tortor, facilisis in neque sit amet, scelerisque elementum tortor. Nullam eget nibh
-              sit amet odio lobortis ullamcorper. Nulla bibendum magna nec sem pulvinar lobortis.
-              Mauris et imperdiet urna, vitae lobortis dui. Nunc elementum elit mi, non mattis enim
-              congue at. Proin mi lectus, ullamcorper id hendrerit eu, ultricies vitae lacus. Nunc
-              vehicula, erat eget laoreet condimentum, felis ante malesuada leo, nec efficitur diam
-              nisi eget nisi. Cras arcu lacus, tristique in bibendum vitae, elementum eget lorem.
-              Maecenas vestibulum volutpat orci eu pharetra. Praesent vel blandit ante, nec faucibus
-              libero. Sed ultrices lorem ex, eu facilisis libero convallis ac. Vivamus id dapibus
-              eros. Nullam tempor sem rhoncus porta semper. Proin bibendum vulputate nisl, fringilla
-              interdum elit pulvinar eu. Quisque vitae quam dapibus, vestibulum mauris quis, laoreet
-              massa.
+              With the company growing on a rapid incline, we are pleased to introduce to you Eddy! 
+              This app will help you find your mentor, mentee, or colleague that will help you to stay connected while working remote.
+              The restraints and devastation caused to all nations from the Pandemic has brought on a massive concern for health and the safety for ourselves and loved ones. 
+              </p>
+              <p>
+              We understand.
+              <br></br>
+              We hear you. 
+              <br>
+              </br>
+              Let us unite and let us help.
+              </p>
+              <p>
+              Everyone affected by the pandemic can confidently say that the adaptation from working in an office, with ALL the resources, to working from home, with limited resources, has created alot of stress. 
+              </p>
+              <p>
+              Who do I call? 
+              <br>
+              </br>
+              Is he/she still employeed? 
+              <br></br>
+              Who can I ask for work advice?
+              </p>
+              <p>
+              This is where Eddy comes in. This App will help you reach the employee in a quick search. 
+              We want to ensure that all the necessary tools and resources are provided to increase your maximum productivity. 
             </p>
           </Col>
         </Row>
