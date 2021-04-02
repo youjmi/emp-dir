84209ea0522b3b5f59a2bd3513dc6a21176c6a25
Young Ji Kim <youjmik@gmail.com>
2021-04-02 01:53:13 -0400

    Changing Link name

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,7 +7,7 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <Link className="navbar-brand" to="/">
-        Employee Directory
+        Eddy
       </Link>
       <div>
         <ul className="navbar-nav">
